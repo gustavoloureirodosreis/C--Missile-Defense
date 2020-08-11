6e4b53284c4f38de20d21d24cd361b48971ca9a8
Gustavo <gusts92@gmail.com>
2020-08-11 00:24:02 -0300

    @basic first script (bullet movement)

--- bullet.cs
+++ bullet.cs
@@ -1,21 +1,18 @@
 using Godot;
 using System;
 
-public class bullet : Area2D
-{
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+public class bullet : Area2D {
+
+    public int speed = 300;
+    Vector2 velocity = new Vector2(0, 0);
 
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
+    public override void _Ready() {
+
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _Process(float _delta) {
+        velocity = Vector2.Right.Rotated(Rotation) * speed * _delta;
+        Translate(velocity);
+     }
 }
