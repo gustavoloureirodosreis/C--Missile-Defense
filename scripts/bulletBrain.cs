c79b55b89e2df7a454f0c10520a9cdd861cdbcb2
Gustavo <gusts92@gmail.com>
2020-08-15 12:25:59 -0300

    @spawning bullets from player cannon

---
+++ scripts/bulletBrain.cs
@@ -0,0 +1,43 @@
+using System;
+using Godot;
+
+public class bulletBrain : Node {
+
+    scenes scenes = new scenes();
+
+    public override void _Ready() {
+
+    }
+
+    public void spawnBullet(Vector2 spawnPosition, Vector2 targetPosition, string animationName) {
+        
+        // Spawn bullet at position, and look at target position
+        var bullet = (bullet)scenes._sceneBullet.Instance();
+        GetNode("/root/game/bullets").AddChild(bullet);
+        bullet.GlobalPosition = spawnPosition;
+        bullet.LookAt(targetPosition);
+
+        // Set the bullet animation
+        var bulletSprite = (AnimatedSprite)bullet.GetNode("AnimatedSprite");
+        bulletSprite.Play(animationName);
+
+    }
+    
+    public void spawnExplosion(Vector2 spawnPosition, Vector2 targetPosition, string animationName) {
+
+         // Spawn explosion at position
+        var explosion = (bullet)scenes._sceneExplosion.Instance();
+        GetNode("/root/game/bullets").AddChild(explosion);
+        explosion.GlobalPosition = spawnPosition;
+
+        // Set the explosion animation
+        var explosionSprite = (AnimatedSprite)explosion.GetNode("AnimatedSprite");
+        explosionSprite.Play(animationName);
+
+    }
+
+	public override void _Process(float delta) {
+
+	}
+
+}
--- scripts/cannonBarrel.cs
+++ scripts/cannonBarrel.cs
@@ -3,9 +3,15 @@ using System;
 
 public class cannonBarrel : Sprite {
 
+    bulletBrain bulletBrain;
+
+    public override void _Ready() {
+        bulletBrain = (bulletBrain)GetNode("/root/game/bullets/bulletBrain");
+    }
+
     public override void _Input(InputEvent _inputEvent) {
         if(_inputEvent.IsActionPressed("click")) {
-            GD.Print("Click click");
+            bulletBrain.spawnBullet(GlobalPosition, GetGlobalMousePosition(), "player");
         }
     }
 
